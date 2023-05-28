dd5c49b6ac41a4b02349ec0ae2949ccd53806cc4
Xtron64 <snekkieboi@pm.me>
2023-05-28 18:44:19 +0100

    Changed documentation

--- snekkie_tools_py/src/Debug/log_bug.py
+++ snekkie_tools_py/src/Debug/log_bug.py
@@ -1,26 +1,22 @@
-# IMPORTS
+'''
+
+'''
 from inspect import currentframe, getframeinfo  # This is required so that we can log the file and line that the function was called at
 import os
 
 
-# DOCUMENTATION
-# The log_bug function logs a bug to 'debug_logs.txt'.
-# This function only works on Linux, and I refuse to add Windows or MacOS support, I would be fine with adding BSD support, or support forany other FOSS operating systems.
-# the log_bug function has two parameters:
-#
-# - description
-# This parameter is the full description of what error occured.
-# This parameter is logged to 'debug_logs.txt'. 
-# It also is raised as an error.
-#
-# - logDir
-# Put the directory of where you want to store 'debug_logs.txt'.
-# Please make sure that it is a valid directory, 
-# you wouldn't want a function used to help with debugging to cause another bug.
-# Also, don't set the value of this parameter to the location of the file,
-# but instead use the directory that it is saved in.
-# This parameter is optional.
-def log_bug(description, logDir=None): 
+'''
+DOCUMENTATION
+The log_bug function logs a bug to 'debug_logs.txt'. This function only works on Linux, and I refuse to add Windows or MacOS support, I would be fine with adding BSD support, or support forany other FOSS operating systems. The log_bug function has two parameters:
+    - description:
+    This parameter is the full description of what error occured. This parameter is logged to 'debug_logs.txt'. It also is raised as an error.
+    - logDir:
+    Put the directory of where you want to store 'debug_logs.txt'. Please make sure that it is a valid directory, you wouldn't want a function used to help with debugging to cause another bug. Also, don't set the value of this parameter to the location of the file, but instead use the directory that it is saved in. This parameter is optional.
+'''
+def log_bug(description, logDir=None) -> None: 
+    '''
+
+    '''
     frameinfo = getframeinfo(currentframe())  # Gets current file and line
     line = frameinfo.lineno  # Stores the line in a variable
     file = frameinfo.filename  # Stores the location of the file in a variable
@@ -39,4 +35,3 @@ def log_bug(description, logDir=None):
             logs.write(log)  # Writes the thing I explained earlier to 'debug_logs.txt'
             logs.close()  # Closes 'debug_logs.txt'
     raise Exception(description)  # Raises an exception so that the interpreter knows that something's wrong
-
